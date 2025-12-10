178f34cbd88cb5236e7b30d371a97a1bf897af22
Bequendionhans <asafesias@gmail.com>
2025-12-09 21:54:08 -0300

    Demonstração de função com 'def'

---
+++ exemplo3.py
@@ -0,0 +1,18 @@
+def saudacao(n, i):
+    print(f'Olá {n}, sua idade é {i}')
+
+
+# nome = input('Informe o nome: ')
+# idade = int(input('Informe a idade: '))
+# saudacao(nome, idade)  #Para replicar o código da linha para baixo use: Shift + Alt + baixo(seta)
+
+
+def soma(n1,n2):
+    total = n1+n2 #sum(n1,n2)
+    print(f'O resultado da soma destes números é: {total}.')
+
+
+
+num1 = float(input('informe o primeiro número: '))
+num2 = float(input('Informe o segundo número: '))
+soma(num1, num2)
